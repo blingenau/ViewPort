b07ea65be40e15f2a786ca81265561991065306d
William Willsey <wwillsey414@gmail.com>
2016-06-16 14:36:38 -0400

    add tab class start

--- src/browser.ts
+++ src/browser.ts
@@ -1,23 +1,91 @@
+class Tab {
+    url: string;
+    id: number;
+    active: boolean;
+    webview: Electron.WebViewElement;
+    constructor (tab: any) {
+        this.url = tab.url || "",
+        this.id = tab.id || Math.round(Math.random() * 100000000000000000),
+        this.webview = tab.webview || null;
+        this.active = tab.active || false;
+        this.webview.addEventListener("dom-ready", () => {
+            debugger;
+        document.getElementById("location-form").onsubmit = (): boolean => {
+            if (this.active) {
+                let address: HTMLInputElement = (<HTMLInputElement>document.getElementById("location"));
+                this.url = address.value;
+                navigateTo(this.webview, this.url);
+            }
+        return false;
+        };
+        this.webview.addEventListener("load-commit", (event: Electron.WebViewElement.LoadCommitEvent) => {
+            let address: HTMLInputElement = (<HTMLInputElement>document.getElementById("location"));
+            address.value = event.url;
+        });
+
+         });
+      }
+  }
+class TabBar {
+    tabs: Tab[];
+    active_tab: number;
+    constructor() {
+        this.tabs = [];
+        this.active_tab = -1 ;
+    }
+    public size(): number {
+        return this.tabs.length;
+    }
+    public add_tab(tab: Tab, background: boolean = false): void {
+        this.tabs.push(tab);
+        if (this.active_tab === -1) {
+            this.active_tab = 0;
+        }
+        if (!background) {
+            // if tab not a background tab then set it as active tab
+            this.active_tab = this.size() - 1;
+        }
+        this.render();
+    }
+    public remove_tab(tab_id: number = -1) {
+        if (this.size() === 0) {
+            console.log("Popping from empty TabBar");
+            return;
+        }
+        if (tab_id === -1) {
+            this.tabs.pop();
+            if (this.active_tab === this.size()) {
+                this.active_tab -= 1;
+            }
+        }
+        else {
+            this.tabs.filter(function (tab: Tab) {
+                return tab.id !== tab_id;
+            });
+        }
+        this.render();
+    }
+    public render(): void {
+        doLayout();
+        // perhaps TabBar should always be non-empty
+        /*
+        let tab: Tab = this.tabs[this.active_tab];
+        tab.webview.addEventListener("load-commit", (event: Electron.WebViewElement.LoadCommitEvent) => {
+        let address: HTMLInputElement = (<HTMLInputElement>document.getElementById("location"));
+        address.value = event.url; 
+    });
+    */
+    }
+}
+
+let Tabs: TabBar = new TabBar();
+
+
 window.onresize = doLayout;
 
-onload = () => {
-    let webview: Electron.WebViewElement = <Electron.WebViewElement>document.getElementById("webpage");
 
-    doLayout();
-    document.getElementById("location-form").onsubmit = (): boolean => {
-        navigateTo((<HTMLInputElement>document.getElementById("location")).value);
-        return false;
-    };
-    webview.addEventListener("load-commit", (event: Electron.WebViewElement.LoadCommitEvent) => {
-        let address: HTMLInputElement = (<HTMLInputElement>document.getElementById("location"));
-
-        address.value = event.url;
-    });
-};
-
-function navigateTo(url: string): void {
+function navigateTo(webview: Electron.WebViewElement, url: string): void {
     let address: HTMLInputElement = (<HTMLInputElement>document.getElementById("location"));
-    let webview: Electron.WebViewElement = <Electron.WebViewElement>document.getElementById("webpage");
 
     if (!url) {
         url = "http://athenanet.athenahealth.com";
@@ -26,7 +94,6 @@ function navigateTo(url: string): void {
     if (url.indexOf("http") === -1) {
         url = `http://${url}`;
     }
-
     address.blur();
     webview.loadURL(url);
 }
