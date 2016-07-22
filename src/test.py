8819f8231c643177507c79a2d69e38b16ccf4857
William Willsey <wwillsey414@gmail.com>
2016-07-22 11:49:19 -0400

    remove test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from sys import stdout
-if __name__ == "__main__":
-    while 1:
-        while not raw_input():
-            pass
-        print "true"
-        stdout.flush()
